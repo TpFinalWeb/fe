6003909cfeb19eb9442cafd1f2bd5b208a1acfb1
yahya mawhoub <yahyamaw311@gmail.com>
2024-12-06 23:03:08 -0500

    deleted useless function

--- src/axios/proxy/userProxy.ts
+++ src/axios/proxy/userProxy.ts
@@ -3,19 +3,6 @@ import { User } from "../models/user.model.ts";
 
 
 export default class UserProxy {
-    // this is a test function to see if the connection is working
-    public async getTestings() {
-        try {
-            return await apiClient.get("/v2/products/").then(
-                res => {
-                    return res.data;
-                }
-            )
-        }catch(error){
-            console.log(error);
-        }
-    }
-
 
     public static async register(user: User): Promise<any> {
         try{
